349d95e88af36b7d88a44eeba8c6fea7e80ad2eb
唐莹莹 <glimmeryi@163.com>
2016-12-27 13:16:25 +0800

    Update blawhipic.py

--- 4-作业提交/唐莹莹_IAT/blawhipic.py
+++ 4-作业提交/唐莹莹_IAT/blawhipic.py
@@ -1,4 +1,4 @@
-#来自 http://www.jiadingqiang.com/2946.html
+#来自 http://lovenight.github.io/2015/11/15/Python-3-%E5%A4%9A%E7%BA%BF%E7%A8%8B%E4%B8%8B%E8%BD%BD%E7%99%BE%E5%BA%A6%E5%9B%BE%E7%89%87%E6%90%9C%E7%B4%A2%E7%BB%93%E6%9E%9C/
 
 import requests
 import re
