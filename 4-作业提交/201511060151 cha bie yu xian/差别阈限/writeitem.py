8af8cef7e1e168cfc51e9fa615926a190b24c959
szhihao <734303717@qq.com>
2017-01-27 20:49:54 +0800

    Add files via upload

---
+++ 4-作业提交/201511060151 cha bie yu xian/差别阈限/writeitem.py
@@ -0,0 +1,14 @@
+﻿#生成iquisit<item>
+
+import os
+
+work_dir="C:/Users/Szh/Desktop/差别阈限/Image/"
+
+PicName = os.listdir(work_dir)
+print("<item pic>")
+j = 0
+for i in range(0,len(PicName)-1):
+        j = i+1
+        print('    ' + '/' + str(j) + ' ' + '=' + ' ' + '"' + PicName[j] + '"')
+print("</item>")
+print("\n")
