bb919c45eeded5665bdd22518f569840b531e3d6
szhihao <734303717@qq.com>
2017-01-27 20:44:17 +0800

    Add files via upload

---
+++ 4-作业提交/201511060151 IAT/人种IAT/writeitem.py
@@ -0,0 +1,28 @@
+﻿#生成inquisit<item>
+
+import os
+
+work_dir = "C:/Users/Szh/Desktop/人种IAT/image/"
+
+os.getcwd()
+
+os.chdir(work_dir)
+picturename = os.listdir(work_dir + "white\\")
+
+print("<item targetA>")
+for i in range(0,len(picturename)):
+    j = i+1
+    print('    '+'/'+str(j)+' = '+'"'+'image/'+'white'+'/'+picturename[i]+'"')
+print("</item>")
+print("\n")
+
+
+os.chdir(work_dir)
+picturename = os.listdir(work_dir + "black\\")
+
+print("<item targetB>")
+for i in range(0,len(picturename)):
+    j = i+1
+    print('    '+'/'+str(j)+' = '+'"'+'image/'+'black'+'/'+picturename[i]+'"')
+print("</item>")
+print("\n")
